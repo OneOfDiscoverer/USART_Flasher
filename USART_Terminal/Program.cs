b47d6368f002eb323046346e1357ebcb42cac168
OneOfDiscoverer <54298157+OneOfDiscoverer@users.noreply.github.com>
2020-01-29 21:56:25 +0700

    Initial commit

---
+++ USART_Terminal/Program.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.IO;
+using System.IO.Ports;
+using System.Threading.Tasks;
+using System.Timers;
+
+
+namespace USART_Terminal
+{
+    class Program
+    {
+        static public string[] FindPorts;
+        static SerialPort port = new SerialPort();
+        static string log_name;
+        static FileStream fs;
+        static byte[] _File;
+       // static string to_send;
+        static string receive;
+        enum State
+        {
+            wait,
+            send_reset,
+            wait_boot_enter,
+            sending_firm,
+            calc_all,
+            send_com_to_ready,
+            entering_boot,
+            All_done,
+            send_page,
+            crc_get,
+            page_get,
+            calc_all_get,
+            addr_get,
+        }
+        static State state = State.wait;
+        static void Main(string[] args)
+        {
+
+            Task task = new Task(() => com());
+            Task flash = new Task(() => Flash_state_mashine());
+            FindPorts = SerialPort.GetPortNames();
+            foreach (string s in FindPorts) Console.WriteLine(s);
+            Console.WriteLine("Enter port name:");
+            port.PortName = "COM6";// Console.ReadLine();
+            //Console.WriteLine(port.PortName + " selected, enter baudrate:");
+            //port.BaudRate = int.Parse(Console.ReadLine());
+            port.BaudRate = 2000000;
+            Console.WriteLine(port.BaudRate + " selected");
+            port.Open();
+            Console.WriteLine(port.PortName + " opened");
+            log_name = DateTime.Now.ToString();
+            log_name = log_name.Replace(".", "");
+            log_name = log_name.Replace(":", "");
+            log_name = log_name.Replace(" ", "_");
+            log_name += ".txt";
+            fs = File.Create(log_name);
+            _File = File.ReadAllBytes("sensors.bin");
+            
+            task.Start();
+            flash.Start();
+            Console.WriteLine(port.PortName + " listening");
+            while (true)
+            {
+                if(state == State.wait)
+                    switch (Console.ReadKey().KeyChar)
+                    {
+                        case 'c':
+                            Console.Clear();
+                            break;
+                        case 'f':
+                            state = State.entering_boot;
+                            break;
+                        case 'r':
+                            port.Write(BitConverter.GetBytes(0xDE), 0, 1);
+                            break;
+                        case 'j':
+                            port.Write(BitConverter.GetBytes(0x01), 0, 1);
+                            break;
+                    }
+            }
+        }
+        private static Task com()
+        {
+            while (true)
+            {
+                receive = port.ReadLine();
+                string outp = DateTime.Now.ToLongTimeString()+ ":" + DateTime.Now.Millisecond.ToString() + ";" + receive + "\n";
+                byte[] bt = System.Text.Encoding.UTF8.GetBytes(outp);
+                Console.Write(outp);
+                if (log_name != null) fs.Write(bt, 0, bt.Length);
+            }
+        }
+        static public void TimerElapsedEventHandler(object sender, EventArgs args) //проверить таймаут
+        {
+            state = State.wait;
+            Console.WriteLine("Timeout");
+        }
+        private static Task Flash_state_mashine()
+        {
+            Timer timer = new Timer(30000);
+            ushort shift = 0;
+            byte[] to_crc = new byte[1024];
+            Crc32 crc32 = new Crc32();
+            uint crc;
+            timer.Elapsed += new ElapsedEventHandler(TimerElapsedEventHandler);
+            while (true)
+            {
+                switch (state)
+                {
+                    case State.wait:
+                        timer.Stop();
+                        break;
+                    case State.entering_boot:
+                        timer.Start();
+                        state = State.send_reset;
+                        break;
+                    case State.send_reset:
+                        port.Write(BitConverter.GetBytes(0x55), 0, 1);
+                       // receive = null;
+                        state = State.wait_boot_enter;
+                        break;
+                    case State.wait_boot_enter:
+                        if (receive == "Boot")  state = State.send_com_to_ready;
+                        break;
+                    case State.send_com_to_ready:
+                        if (shift * 0x400 < _File.Length)
+                        {
+                            port.Write(BitConverter.GetBytes(0x02), 0, 1);
+                            state = State.sending_firm;
+                        }
+                        else state = State.All_done;
+                        break;
+                    case State.sending_firm:
+                        if (receive == "ready")
+                        {
+                            byte[] buf = new byte[4];
+                            port.Write(BitConverter.GetBytes(0x08002000 + shift * 0x400), 0, 4);
+                            port.Write(BitConverter.GetBytes(0x08002000 + shift * 0x400 + 0x400), 0, 4);
+                            state = State.addr_get;
+                        }
+                        break;
+                    case State.addr_get:
+                        if (receive == "addr get")
+                        {
+                            for (int i = 0; i < 1024; i++)
+                            {
+                                if (i + shift * 0x400 < _File.Length) to_crc[i] = _File[i + shift * 0x400];
+                                else to_crc[i] = 0xFF;
+                            }
+                            crc = crc32.GetCRC32B(to_crc);
+                            port.Write(BitConverter.GetBytes(crc), 0, 4);
+                            state = State.crc_get;
+                        }
+                        break;
+                    case State.crc_get:
+                        if (receive == "crc get")
+                        {
+                            port.Write(to_crc, 0, 0x400);
+                            Console.WriteLine("page " + shift.ToString());
+                            state = State.page_get;
+                        }
+                        break;
+                    case State.page_get:
+                        if (receive == "Page writed")
+                        {
+                            shift++;
+                            state = State.send_com_to_ready;
+                        }
+                        break;
+                    case State.All_done:
+                        Console.WriteLine("final crc calc");
+                        port.Write(BitConverter.GetBytes(0x04), 0, 1);
+                        state = State.calc_all;
+                        break;
+                    case State.calc_all:
+                        byte[] all_crc = new byte[shift * 0x400];
+                        if (receive == "ready")
+                        {
+                            for (int i = 0; i < shift * 0x400; i++)
+                            {
+                                if (i < _File.Length) all_crc[i] = _File[i];
+                                else all_crc[i] = 0xFF;
+                            }
+                            crc = crc32.GetCRC32B(all_crc);
+                            port.Write(BitConverter.GetBytes(0x08002000), 0, 4);
+                            port.Write(BitConverter.GetBytes(0x08002000 + shift * 0x400), 0, 4);
+                            port.Write(BitConverter.GetBytes(crc), 0, 4);
+                            shift = 0;
+                            state = State.wait;
+                        }
+                        break;
+                    default:
+                        state = State.wait;
+                        break;
+                }
+            }
+        }
+    }
+    
+
+}
---
+++ USART_Terminal/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("USART_Terminal")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("USART_Terminal")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("7a54ab9a-0b90-4231-b67b-7b2059498a0f")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
